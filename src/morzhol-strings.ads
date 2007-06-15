d302c669e8ba17479a1c11fd610dae31eb70c5c0
olivier.ramonat <olivier.ramonat@6163e170-e132-0410-82d0-65ffa3ac32ee>
2007-06-15 14:08:49 +0000

    Adds "+" and "-" aliases for To_Unbounded_String and To_String
    git-svn-id: https://morzhol.googlecode.com/svn/trunk@3 6163e170-e132-0410-82d0-65ffa3ac32ee

---
+++ src/morzhol-strings.ads
@@ -0,0 +1,34 @@
+------------------------------------------------------------------------------
+--                                Morzhol                                   --
+--                                                                          --
+--                           Copyright (C) 2007                             --
+--                      Pascal Obry - Olivier Ramonat                       --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.       --
+------------------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+
+package Morzhol.Strings is
+
+   use Ada.Strings.Unbounded;
+
+   function "+" (Source : String) return Unbounded_String
+                 renames To_Unbounded_String;
+
+   function "-" (Source : Unbounded_String) return String
+                 renames To_String;
+
+end Morzhol.Strings;
